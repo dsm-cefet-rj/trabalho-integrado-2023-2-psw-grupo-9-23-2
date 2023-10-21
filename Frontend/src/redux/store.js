44c1b3f1c7dfb365422543708a80ece3dec435c9
Matheus Amendoeira <mathamenrj@gmail.com>
2023-10-21 13:51:26 -0300

    start no redux (nada implementado)

---
+++ Frontend/src/redux/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+export const store = configureStore({
+  reducer: {
+      carro:carroReducer,
+      horario:horarioReducer,
+      filtro:filtroReducer
+  },
+});
