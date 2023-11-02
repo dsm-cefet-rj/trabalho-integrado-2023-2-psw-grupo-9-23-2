4cab60e61f20de7eaa7058326efdfe7ef55736db
JoelRTavares <108430269+JoelRTavares@users.noreply.github.com>
2023-11-02 15:35:21 -0300

    Update dos filtros
    O próximo passo agora é realizar as rotas dos filtros

--- Frontend/src/Filtros/CriadorFiltros.jsx
+++ Frontend/src/Filtros/CriadorFiltros.jsx
@@ -4,77 +4,7 @@ import React, {useState, useEffect} from 'react'
 import Cabecalho from '../Layout/Cabecalho';
 export default function CriadorFiltros(props)
 {
-    /*
-    const [marcas, setMarca] = useState({
-        marca: '',       
-      });
-      const handleInputChange = (e) => {
-        const { name, value } = e.target;
-        setMarca({
-            ...marcas,
-          [name] : value
-        });
-      };
-        
-      useEffect(() => {
-        fetch('http://localhost:8000/filtros')
-          .then((response) => response.json())
-          .then((data) => {
-            setMarca(data);
-          })
-          .catch((error) => {
-            console.error('Error fetching data:', error);
-          });
-      }, []);
-      const handleSubmit = async (e) => {
-        e.preventDefault();
     
-        try {
-          const newEntry = {
-            marca: marcas.marca, 
-            id: marcas.length + 1     
-          };
-    
-          const response = await fetch('http://localhost:8000/filtros', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(newEntry),
-          });
-    
-          if (response.ok) {
-            const responseData = await response.json();
-            console.log('New entry created:', responseData);
-    
-            setMarca({
-                marca: '',               
-              });
-          } else {
-            console.error('Error creating new entry:', response.status, response.statusText);           
-          }
-        } catch (error) {
-            alert('Essa marca já existe nos filtros!');
-            console.log(error);
-        }
-      };
-    return(
-        <>
-            <form onSubmit={handleSubmit}>
-                <div className="input-group mb-3">
-                    <label htmlFor="marca">Insira nome de marca: </label>
-                    <input type="text" className="form-control" 
-                    id="marca"
-                    name="marca"
-                    value={marcas.marca}
-                    onChange={handleInputChange}
-                    />
-                    <button className="btn btn-outline-secondary" type="submit">Adicionar marca</button>
-                </div>
-            </form>
-        </>
-    );
-    */
     const [formData, setFormData] = useState({
         marca: '',
         
@@ -104,9 +34,20 @@ export default function CriadorFiltros(props)
         e.preventDefault();
     
         try {
+
+          if(mar.filter((m)=> m.marca === formData.marca).length > 0)
+          {
+            alert('Essa marca já existe nos filtros!');
+            return;
+          }
+          else if(formData.marca == "")
+          {
+            alert('Insira o nome da marca a ser criada');
+            return;
+          }
           const newEntry = {
             ...formData,
-            id: formData.marca
+                        
           };
     
           const response = await fetch('http://localhost:8000/filtros', {
@@ -134,7 +75,7 @@ export default function CriadorFiltros(props)
                   console.error('Error fetching data:', error);
                 });
               
-              
+              window.location.reload();  
           } else {
             console.error('Error creating new entry:', response.status, response.statusText);
             if(response.status == 500)
