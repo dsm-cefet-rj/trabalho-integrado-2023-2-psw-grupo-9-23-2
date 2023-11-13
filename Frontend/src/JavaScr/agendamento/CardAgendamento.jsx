d4a7400552ad4bfa534e9594db8cdf104f440bfd
JoelRTavares <108430269+JoelRTavares@users.noreply.github.com>
2023-11-13 16:01:55 -0300

    Relacionamento agendamento X Carros quase completo
    Agora aparece o nome do carro no card agendamento. Não consegui fazer usando populate, mas com uma lógica dentro do proprio card de agendamento para selecionar o carro e pegar seu nome

--- Frontend/src/JavaScr/agendamento/CardAgendamento.jsx
+++ Frontend/src/JavaScr/agendamento/CardAgendamento.jsx
@@ -1,7 +1,31 @@
 import '/bootstrap-5.3.1-dist/css/bootstrap.css'
-
+import React, { useEffect, useState } from 'react';
 export default function CardAgendamento(props){
-  let aux = props.carroRef != null ? true : false;
+  const aux = props.carroRef != null ? true : false;
+  const [carros, setCarros] = useState([]);
+  let car;
+  if(aux)
+  {
+    useEffect(() => {
+      fetch('http://localhost:8000/carros')
+        .then((response) => response.json())
+        .then((data) => {
+          setCarros(data);
+        })
+        .catch((error) => {
+          console.error('Error fetching data:', error);
+        });
+    }, []);
+    car = carros.filter(
+      (c) => c.id == props.carroRef
+    );
+    
+    console.log(car);
+    if(car.length <= 0)
+    {
+      car.nome = "Carro inexistente ou deletado.";
+    }
+  }
   return(
   <>
     <div className="col-xl-2 col-lg-3 col-md-4 col-sm-6 ">
@@ -9,7 +33,7 @@ export default function CardAgendamento(props){
       <div className="card text-center bg-light ">
         
         <div className="card-body">
-          <p className="card-text">Dia: {props.data}<br></br>Horário: {props.hora}<br></br></p>{aux && <p>Carro: {props.carroRef}</p>}
+          <p className="card-text">Dia: {props.data}<br></br>Horário: {props.hora}<br></br></p>{aux && <p>Carro: {car[0].nome}</p>}
         </div>
         <div className="card-footer">
         </div>
