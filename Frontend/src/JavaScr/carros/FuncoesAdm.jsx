fa000f199cd6ed1820256d03cabd1aba33458dba
Matheus Amendoeira <mathamenrj@gmail.com>
2023-12-03 11:09:40 -0300

    trocando de lugar o admin

--- Frontend/src/JavaScr/carros/FuncoesAdm.jsx
+++ Frontend/src/JavaScr/carros/FuncoesAdm.jsx
@@ -7,22 +7,11 @@ export default function FuncoesAdm()
         <>
             <div className='container'>
                 <h2>Visão de Administrador:</h2>
-                <h4>Gerenciar carros: </h4>
-                <div className='col'>
-                    <Link to="/CriadorCarro"><button className='btn btn-info'>Criar Carro</button></Link>
-                    <Link to="/UpdaterCarro"><button className='btn btn-info'>Modificar Carro</button></Link>
-                    <Link to="/DeleterCarro"><button className='btn btn-info'>Apagar Carro</button></Link>
-                </div>
                 <h3>Gerenciar filtros: </h3>
                 <div className='col'>
                     <Link to="/filtros-adm"><button className='btn btn-info'>Gerenciar marcas</button></Link>                  
                 </div>
-                <h3>Gerenciar agendamentos: </h3>
-                <div className='col'>
-                    <Link to="/checar-agenda"><button className='btn btn-info'>Checar agendamentos</button></Link>
-                    <Link to="/criador-agenda"><button className='btn btn-info'>Criar agendamentos</button></Link>                  
-                    <Link to="/deletar-agenda"><button className='btn btn-info'>Deletar agendamentos</button></Link>
-                </div>
+                
             </div>
         </>
         
